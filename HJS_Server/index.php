f0eade73695ddd86f87f3b8fbcb1f1f13d31e2c7
mrhery <aimanmrc@gmail.com>
2018-05-04 09:50:12 +0800

    Update index.php

--- HJS_Server/index.php
+++ HJS_Server/index.php
@@ -1,1 +1,14 @@
+<?php
+;"Hery JSON-Sql Statement API";
+;"Developed By Mr Hery";
+;"Early of May 2018";
+;"At Intelligent Hosting Pte. Ltd. HQ Office Malaysia";
 
+#Error Controller
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define("HJS", true);
+require_once("setup/startup_library.php");
+?>
