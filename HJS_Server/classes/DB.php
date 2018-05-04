0caa61329ac4c588bd0bc92b32b09c8721933f50
mrhery <aimanmrc@gmail.com>
2018-05-04 09:50:41 +0800

    Create DB.php

---
+++ HJS_Server/classes/DB.php
@@ -0,0 +1,174 @@
+<?php
+require_once(dirname(__DIR__) . "/controller/document_access.php");
+
+class DB{
+	private static $_instance = null;
+	private $_pdo, $_query, $_error = false, $_results, $_count = 0;
+	
+	private function __construct($data = array()){
+		try{
+			if(!empty($data["host"]) AND !empty($data["database"]) AND !empty($data["username"]) AND !empty($data["password"])){
+				$this->_pdo = new PDO("mysql:host=" . $data["host"] . "; dbname=" . $data["database"],$data["username"], $data["password"]);
+			}else{
+				$this->_pdo = new PDO("mysql:host=" . Config::get("mysql/host") . "; dbname=" . Config::get("mysql/db"), Config::get("mysql/username"), Config::get("mysql/password"));
+			}
+		}catch(PDOException $ex){
+			die($ex->getMessage());
+		}
+	}
+	
+	public static function conn($data = array()){
+		if(!isset(self::$_instance)){
+			self::$_instance = new DB($data);
+		}
+		return self::$_instance;
+	}
+	
+	public function query($sql, $params){
+		$this->_error = false;
+		if($this->_query = $this->_pdo->prepare($sql)){
+			$x = 1;
+			if(count($params)){
+				foreach($params as $param){
+					$this->_query->bindValue($x, $param);
+					$x++;
+				}
+			}
+			
+			if($this->_query->execute()){
+				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+				$this->_count = $this->_query->rowCount();
+			}else{
+				$this->_error = true;
+			}
+		}
+		return $this;
+	}
+	
+	public function update($table, $where, $fields){
+		$set = '';
+		$x = 1;
+		
+		foreach($fields as $name => $value){
+			$set .= "{$name} = ?";
+			if($x < count($fields)){
+				$set .= ", ";
+			}
+			$x++;
+		}
+		
+		$sql = "UPDATE {$table} SET {$set} WHERE {$where}";
+		if($this->query($sql, $fields)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public function insert($table, $fields = array()){
+		if(count($fields)){
+			$keys = array_keys($fields);
+			$values = '';
+			$x = 1;
+			
+			foreach($fields as $field){
+				$values .= '?';
+				if($x < count($fields)){
+					$values .= ',';
+				}
+				$x++;
+			}
+			
+			$sql = "INSERT INTO {$table} (`" . implode('`, `', $keys) . "`) VALUES({$values})";
+			
+			if($this->query($sql, $fields)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public function del($table, $column, $data){
+		$sql = "DELETE FROM  {$table} WHERE {$column} = {$data}";
+		if($this->_query = $this->_pdo->prepare($sql)){
+			if($this->_query->execute()){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	
+	public function settingInfo(){
+		$this->_error = false;
+		$sql = "SELECT * FROM settings";
+		if($this->_query = $this->_pdo->prepare($sql)){
+			if($this->_query->execute()){
+				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+				return $this->_results;
+			}else{
+				$this->_error = false;
+			}
+		}else{
+			echo "Error Preparing the statement.";
+		}
+	}
+	
+	public function getData($data, $table, $column){
+		$sql = "SELECT * FROM " . $table . " WHERE " . $column . " = " . $data;
+		if($this->_query = $this->_pdo->prepare($sql)){
+			if($this->_query->execute()){
+				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+				return $this->_results;
+			}else{
+				$this->_error = false;
+			}
+		}else{
+			$this->_error = true;
+		}
+	}
+	
+	public function isDuplicate($data, $table, $column){
+		$sql = "SELECT * FROM {$table} WHERE {$column} = {$data}";
+		if($this->_query = $this->_pdo->prepare($sql)){
+			if($this->_query->execute()){
+				$this->_count = $this->_query->rowCount();
+				return $this;
+			}
+		}else{
+			$this->_error = true;
+		}
+		return $this;
+	}
+	
+	public function q($sql){
+		$this->_error = false;
+		if($this->_query = $this->_pdo->prepare($sql)){
+			if($this->_query->execute()){
+				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+				$this->_count = $this->_query->rowCount();
+			}else{
+				$this->_error = true;
+			}
+		}
+		return $this;
+	}
+	
+	
+	public function results(){
+		return $this->_results;
+	}
+	
+	public function error(){
+		return $this->_error;
+	}
+	
+	public function count(){
+		return $this->_count;
+	}
+	
+}
+
+?>
